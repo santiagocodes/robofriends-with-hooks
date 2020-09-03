8c8179ccc346a90d3876bb0c500e1b5e7be2bc54
mc-santiago <mari.c.santiago@europe.com>
2020-09-03 20:40:02 +0200

    folder structure

---
+++ src/containers/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+// import { robots } from './robots.js';
+import './App.css';
+
+export default class App extends Component {
+   constructor() {
+      super();
+      this.state = {
+         // robots: robots,
+         robots: [],
+         searchField: '',
+      };
+   }
+
+   componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+         .then((response) => response.json())
+         .then((users) => this.setState({ robots: users }));
+   }
+
+   onSearchChange = (e) => {
+      this.setState({ searchField: e.target.value });
+   };
+
+   render() {
+      const { robots, searchField } = this.state;
+      const filteredRobots = robots.filter((robot) => {
+         return robot.name.toLowerCase().includes(searchField.toLowerCase());
+      });
+      return !robots.length ? (
+         <h1 className="tc">Loading...</h1>
+      ) : (
+         <div className="tc">
+            <h1 className="f1">RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+            <Scroll>
+               <CardList robots={filteredRobots} />
+            </Scroll>
+         </div>
+      );
+   }
+}
